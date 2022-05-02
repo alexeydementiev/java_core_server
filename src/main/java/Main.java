7add3f9be1b9729ed67689dccff8a829b46af76e
alexeydementiev <adementiev@flyxo.com>
2022-05-02 22:56:09 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		int port = 8099;
+		ServerSocket serverSocket = new ServerSocket(port);
+		System.out.println("Server is working...");
+
+		while (true) {
+			Socket clientSocket = serverSocket.accept();
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			final String name = in.readLine();
+
+			out.println(String.format("Your name is %s, your port is %d", name, clientSocket.getPort()));
+		}
+	}
+}
---
+++ src/main/java/classes/Client.java
@@ -0,0 +1,25 @@
+package classes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) throws IOException {
+		int port = 8099;
+		String host = "netology.homework";
+
+		try (Socket clientSocket = new Socket(host, port);
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+			out.println("Petr");
+			String responce = in.readLine();
+			System.out.println(responce);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
