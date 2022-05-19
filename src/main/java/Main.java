bc4f3674511b0dab28e61208bbe0ac2542f132a6
alexeydementiev <adementiev@flyxo.com>
2022-05-19 16:59:04 +0300

    Added try with resources for clientSocket

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,18 +9,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		int port = 8099;
-		try (ServerSocket serverSocket = new ServerSocket(port);) {
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
 
 			System.out.println("Server is working...");
 
 			while (true) {
-				Socket clientSocket = serverSocket.accept();
-				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				try (Socket clientSocket = serverSocket.accept()) {
+					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+					BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
 
 				final String name = in.readLine();
 
 				out.println(String.format("Your name is %s, your port is %d", name, clientSocket.getPort()));
+				}  catch (IOException exception) {
+					exception.printStackTrace();
+				}
 			}
 		} catch (IOException exception) {
 			exception.printStackTrace();
